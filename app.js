ef0e8a027bf53a6270a35c73743669734d8ec3d5
Alex Taylor <altays633@gmail.com>
2020-04-03 11:34:56 -0400

    adding inquirer questions with logic for adding more employees and specifying type of employee

--- app.js
+++ app.js
@@ -1,63 +1,92 @@
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
-// const inquirer = require("inquirer");
-// const path = require("path");
-// const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-// ​
-// const render = require("./lib/htmlRenderer");
-​​
-// inquirer.prompt([
-//         {
-//             type: `input`,
-//             name: `Nname`,
-//             message: `Please enter your name`
-//         },
-//         {
-//             type: `input`,
-//             name: `ID`,
-//             message: `Please enter your ID`
-//         },
-//         {
-//             type: `input`,
-//             name: `email`,
-//             message: `Please enter your email`
-//         },
-//     ])
-//     .then(data => {
-//        let inquirerData = data;
-//        console.log(inquirerData + " inside .then")
-//     //    return inquirerData;
-//     })
-//     .catch(error => {
-//         console.log(error)
-//     })
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+let output = []
+
+const questions = [
+    {
+        type: `input`,
+        name: `name`,
+        message: `Please enter your name`
+    },
+    {
+        type: `input`,
+        name: `ID`,
+        message: `Please enter your ID`
+    },
+    {
+        type: `input`,
+        name: `email`,
+        message: `Please enter your email`
+    },
+    {
+        type: `list`,
+        name: `role`,
+        message: `Please enter your role`,
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is your office number?',
+        when: function(answers) {
+          return answers.role === "Manager";
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github?',
+        when: function(answers) {
+          return answers.role === "Engineer";
+        }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is your school?',
+        when: function(answers) {
+          return answers.role === "Intern";
+        }
+    },
+    {
+        type: `confirm`,
+        name: `addMore`,
+        message: `Do you want to add more employees?`
+    },
+    // logic to restart questions
+]
+
+function ask() {
+    inquirer.prompt(questions).then(answers => {
+        output.push(answers);
+        if (answers.addMore) {
+            ask();
+        }  else {
+            console.log(`You have entered ${output.length} employees.`)
+            // console.log(JSON.stringify(output));
+            return output
+        }
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+ask();
 
 
-// console.log(inquirerData + " outside .then")
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
-
-
-console.log("hello!")
